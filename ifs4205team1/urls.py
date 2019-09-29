534a4d5de41566c344af40cd6df5831e18176a38
guekling <yeoyueling@gmail.com>
2019-09-29 04:25:31 -0700

    Add views for show_all_notes and show_note

--- ifs4205team1/urls.py
+++ ifs4205team1/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
+
+from ifs4205team1 import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
     path('patient/', include('patientlogin.urls')),
     path('patient/', include('patientrecords.urls')),
-]
+    path('patient/', include('patienthealthcare.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
