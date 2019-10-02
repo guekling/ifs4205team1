0988522a96c28afa6a8baedcb5e7e4694acb662e
EdwardKSG <edwardkang97@gmail.com>
2019-10-03 06:50:36 +0800

    add 2FA login page

--- patientlogin/forms.py
+++ patientlogin/forms.py
@@ -6,3 +6,6 @@ class UserEditForm(forms.ModelForm):
   class Meta:
     model = User
     fields = ['first_name', 'last_name', 'email', 'dob', 'address', 'postalcode', 'contactnumber']
+
+class UserQrForm(forms.Form):
+  otp = forms.CharField(max_length=6)
--- patientlogin/urls.py
+++ patientlogin/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   path('<uuid:patient_id>/settings/edit', views.patient_edit_settings, name="patient_edit_settings"),
   path('<uuid:patient_id>/settings/change-password/', views.patient_change_password, name="patient_change_password"),
   path('<uuid:patient_id>/settings/change-password-complete/', views.patient_change_password_complete, name="patient_change_password_complete"),
-  path('<uuid:patient_id>/dashboard/', views.patient_dashboard, name="patient_dashboard")
+  path('<uuid:patient_id>/dashboard/', views.patient_dashboard, name="patient_dashboard"),
+  path('<uuid:patient_id>/qr-login/', views.patient_qr, name="patient_qr")
 ]
