bd6213666f7ea7c3cd6f8bb7159e5edc017eab40
guekling <yeoyueling@gmail.com>
2019-10-03 09:27:05 -0700

    Add commands & Generate readings and readingsperm data

--- patientrecords/management/commands/clear_records.py
+++ patientrecords/management/commands/clear_records.py
@@ -1,11 +1,23 @@
 from django.core.management import BaseCommand
 from patientrecords.models import Readings, TimeSeries, Documents, Images, Videos, Diagnosis
 
+MODEL_ALL = 'all'
+
+MODEL_READINGS = 'readings'
+MODEL_TIMESERIES = 'timeseries'
+MODEL_DOCUMENTS = 'documents'
+MODEL_IMAGES = 'images'
+MODEL_VIDEOS = 'videos'
+MODEL_DIAGNOSIS = 'diagnosis'
+
 class Command(BaseCommand):
   help = "DEV COMMAND: Clear all medical records data."
 
+  def add_arguments(self, parser):
+    parser.add_argument('--model', type=str)
+
   def handle(self, *args, **options):
-    clear_all()
+    run(self, options['model'])
         
 def clear_all():
   """
@@ -17,3 +29,19 @@ def clear_all():
   Images.objects.all().delete()
   Videos.objects.all().delete()
   Diagnosis.objects.all().delete()
+
+def run(self, model):
+  if model == MODEL_ALL:
+    clear_all()
+  elif model == MODEL_READINGS:
+    Readings.objects.all().delete()
+  elif model == MODEL_TIMESERIES:
+    TimeSeries.objects.all().delete()
+  elif model == MODEL_DOCUMENTS:
+    Documents.objects.all().delete()
+  elif model == MODEL_IMAGES:
+    Images.objects.all().delete()
+  elif model == MODEL_VIDEOS:
+    Videos.objects.all().delete()
+  else:
+    Diagnosis.objects.all().delete()
--- patientrecords/management/commands/generate_records.py
+++ patientrecords/management/commands/generate_records.py
@@ -1,3 +1,4 @@
+import csv
 import random
 
 from django.core.management import BaseCommand
@@ -7,8 +8,64 @@ from patientrecords.models import Readings, TimeSeries, Documents, Images, Video
 class Command(BaseCommand):
   help = "DEV COMMAND: Seed database with a generated set of medical records data for testing and development purposes."
 
+  def add_arguments(self, parser):
+    parser.add_argument('--csvpath', type=str)
+
   def handle(self, *args, **options):
-    generate_timeseries_records()
+    path = options['csvpath']
+    generate_readings_records(path)
+
+    # generate_timeseries_records()
+
+def generate_readings_records(path):
+  """
+  Load CSV to populate `Readings` & `ReadingsPerm` table with data
+  """
+  with open(path, 'rt') as csvFile:
+    reader = csv.reader(csvFile, delimiter=',', quotechar="\"")
+    fields_name = next(reader)
+    for r, row in enumerate(reader):
+      reading = Readings()
+
+      if (r < 6000): # Readings 1 - 6000 to Patients 1 - 6000
+        temp_r = r
+      elif (r >= 6000) and (r < 12000): # Readings 6001 - 12000 to Patients 1 - 6000
+        temp_r = r - 6000
+      elif (r >= 12000) and (r < 18000): # Readings 12001 - 18000 to Patients 1 - 6000
+        temp_r = r - 12000
+      elif (r >= 18000) and (r < 24000): # Readings 18001 - 24000 to Patients 1 - 6000
+        temp_r = r - 18000
+      elif (r >= 24000) and (r < 30000): # Readings 24001 - 30000 to Patients 1 - 6000
+        temp_r = r - 24000
+      elif (r >= 30000) and (r < 36000): # Readings 30001 - 36000 to Patients 1 - 6000
+        temp_r = r - 30000
+      else: # Readings 36001 - 40000 to Patients 1 - 4000
+        temp_r = r - 36000
+
+      for i, field in enumerate(row):
+        print("{}{}".format("Currently processing ", fields_name[i]))
+        print("{}{}".format("Currently processing ", field))
+        setattr(reading, fields_name[i], field)
+        
+        # Assign Readings to a specific User & Patient
+        patient = Patient.objects.all()[temp_r]
+        user = patient.username
+        reading.owner_id = user
+        reading.patient_id = patient 
+
+      reading.save()
+      print("{}{}{}".format("Reading ", r, " is saved."))
+
+      # Get all patient's healthcare professional
+      patient_healthcare = patient.healthcare_patients.all()
+
+      # Assign permissions to Healthcare
+      for healthcare in patient_healthcare.iterator():
+        perm = ReadingsPerm.objects.create(readings_id=reading, given_by=user, perm_value=2)
+        perm.username.add(healthcare)
+      print("{}{}".format("Finishing processing permissions for reading ", r))
+
+
 
 def generate_timeseries_records():
   """
