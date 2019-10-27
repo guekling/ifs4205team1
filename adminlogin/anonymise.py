8dfc5efb621addd1ce478700581c694e2227cad1
CYX28 <chanyx17@gmail.com>
2019-10-28 04:07:07 +0800

    Add missing files

---
+++ adminlogin/anonymise.py
@@ -0,0 +1,223 @@
+from __future__ import division
+from django.utils import timezone
+from datetime import timedelta
+import pytz
+from core.models import Patient, User
+from patientrecords.models import Diagnosis, Readings, Images, Videos # Production DB
+from researcherquery.models import QiInfo, SafeUsers, SafeDiagnosis, SafeReadings, SafeImages, SafeVideos # SAFE DB
+from adminlogin.anonymise_helper import *
+
+def anonymise_and_store():
+	k_value = 3
+	patients = Patient.objects.all()
+	total_patients = patients.count()
+
+	# Step 1: For each QI combi e.g. (A, P, LM)
+	for key in QI_COMBI:
+		# Step 2: For each set of unique QIs
+		# Step 2a: Retrieve each set of unique QIs from DB
+		# Step 2b: Loop the list
+
+		# Get all users including non-patients with distinct pair of age & postalcode
+		# Only get 1st entry of distinct pairs
+		# Will not get all patients if each set of distinct pair has multiple rows
+		unique_entries = User.objects.distinct('age', 'postalcode').order_by('age', 'postalcode')
+
+		for unique_entry in unique_entries:
+			if unique_entry.is_patient():
+				unique_qi_age = unique_entry.age
+				unique_qi_postalcode = unique_entry.postalcode
+
+				print(unique_qi_age)
+				print(unique_qi_postalcode)
+
+				# Step 3: Generalise QIs (age & postalcode) based on QIs combi
+				# Step 4: Get all users including non-patients with current set of unique QIs (set QIs as filter)
+				users = generalise_and_get_users(QI_COMBI[key], unique_entry)
+				users_copy = users
+
+				for user in users:
+					if user.is_patient():
+						patient = user.patient_username
+
+						# Step 5: Generalise date
+						# Step 6: Check if patient has at least 1 record (readings) within the combi_date (LM, LY or *)
+						# Step 6a: If exists at least 1 record, continue (keep patient)
+						# Step 6b: Else, remove patient from list then continue
+						record_exists = generalise_date_and_check_record_exists(QI_COMBI[key][DATE_NAME], patient)
+
+						if not record_exists:
+							# Remove patient from list
+							users = users.exclude(username=user.username)
+
+					else:
+						users = users.exclude(username=user.username)
+						users_copy = users_copy.exclude(username=user.username)
+
+				# After this for loop:
+				# users = all patients with at least 1 record in current set of unique QIs
+				# users_copy = all patients with or without at least 1 record in current set of unique QIs
+
+				# Step 7: Check if # of patients left >= k
+				# Step 7a: If (patients_left >= k), continue to next set of unique QIs value (same QI combi)
+				# Step 7b: Else, suppress all patients with this set of unique QIs
+				patients_left = users.count()
+
+				# patients = all patients
+
+				if not (patients_left >= k_value):
+					for user in users_copy:
+						patients = patients.exclude(username=user.username)
+
+				# After this if:
+				# patients = all patients that satisfy at least k patients in current set of unique QIs
+
+		# Step 8: Compute suppression rate
+		# Step 8a: If suppression rate <= 0.1, done (exit loop and store this QIs combi in SAFE DB)
+		# Step 8b: Else, continue to next QIs combi
+		total_patients_left = patients.count()
+		suppression_rate = ((total_patients - total_patients_left) / total_patients) * 100
+
+		print("TOTAL PATIENT")
+		print(total_patients)
+		print(total_patients_left) #COMPARE WITH THIS
+		print(suppression_rate)
+
+		if suppression_rate <= 10.0:
+			# Step 9: Store anonymised records into SAFE DB
+			store_qi_combi(QI_COMBI[key], k_value, suppression_rate)
+			store_anonymised_records(QI_COMBI[key], patients)
+			break
+
+def store_qi_combi(qi_combi, k_value, suppression_rate):
+	combi_age = qi_combi[AGE_NAME]
+	combi_postalcode = qi_combi[POSTALCODE_NAME]
+	combi_date = qi_combi[DATE_NAME]
+
+	print("STORE_QI_COMBI")
+
+	# Remove all data from table if not empty
+	qiinfo_data = QiInfo.objects.all()
+	if qiinfo_data.count() != 0:
+		QiInfo.objects.all().delete()
+
+	QiInfo.objects.create(combi_age=combi_age, combi_postalcode=combi_postalcode, combi_date=combi_date, k_value=k_value, suppression_rate=suppression_rate)
+
+	print("DONE_SAVING_QI_COMBI")
+
+	# print(qiinfo_obj.id)
+
+def store_anonymised_records(qi_combi, patients):
+	# Remove all data from tables if not empty
+	safereadings_data = SafeReadings.objects.all()
+	if safereadings_data.count() != 0:
+		SafeReadings.objects.all().delete()
+
+	safeusers_data = SafeUsers.objects.all()
+	if safeusers_data.count() != 0:
+		SafeUsers.objects.all().delete()
+
+	print("STORE_ANONYMISED_RECORDS")
+
+	for patient in patients:
+		user = User.objects.get(username=patient.username)
+
+		# Convert age & postalcode based on current QI combi
+		if qi_combi[AGE_NAME] == COMBI_AGE_DECADE:
+			user.age = map_age_to_decade(user.age)
+		if qi_combi[AGE_NAME] == COMBI_AGE_ALL:
+			user.age = '*'
+
+		if qi_combi[POSTALCODE_NAME] == COMBI_POSTALCODE_SECTOR:
+			user.postalcode = user.postalcode[:2] + 'XXXX'
+		if qi_combi[POSTALCODE_NAME] == COMBI_POSTALCODE_ALL:
+			user.postalcode = '*'
+
+		safeusers_obj = SafeUsers.objects.create(uid=user.uid, age=user.age, postalcode=user.postalcode)
+
+		# print(safeusers_obj.uid)
+
+		# Get all readings associated with current patient
+		readings = patient.readings_patient.all()
+		for reading in readings:
+			safereadings_obj = SafeReadings.objects.create(uid=safeusers_obj, type=reading.type, value=reading.data)
+
+			# print(safereadings_obj.id)
+
+	print("DONE_SAVING_RECORDS")
+
+def generalise_and_get_users(qi_combi, user):
+	combi_age = qi_combi[AGE_NAME]
+	combi_postalcode = qi_combi[POSTALCODE_NAME]
+
+	if combi_age == COMBI_AGE_EXACT and combi_postalcode == COMBI_POSTALCODE_EXACT:
+		users = User.objects.filter(age=user.age, postalcode=user.postalcode).order_by('age', 'postalcode')
+		return users
+
+	if combi_age == COMBI_AGE_EXACT and combi_postalcode == COMBI_POSTALCODE_SECTOR:
+		postalcode = str(user.postalcode)
+		postalcode = postalcode[:2]
+		users = User.objects.filter(age=user.age, postalcode__startswith=postalcode).order_by('age', 'postalcode')		
+		return users
+
+	if combi_age == COMBI_AGE_EXACT and combi_postalcode == COMBI_POSTALCODE_ALL:
+		users = User.objects.filter(age=user.age).order_by('age', 'postalcode')
+		return users
+
+	if combi_age == COMBI_AGE_DECADE and combi_postalcode == COMBI_POSTALCODE_EXACT:
+		ages = generalise_age_decade(user.age)
+		users = User.objects.filter(age__in=ages, postalcode=user.postalcode).order_by('age', 'postalcode')		
+		return users
+
+	if combi_age == COMBI_AGE_DECADE and combi_postalcode == COMBI_POSTALCODE_SECTOR:
+		ages = generalise_age_decade(user.age)
+		postalcode = str(user.postalcode)
+		postalcode = postalcode[:2]
+		users = User.objects.filter(age__in=ages, postalcode__startswith=postalcode).order_by('age', 'postalcode')
+		return users
+
+	if combi_age == COMBI_AGE_DECADE and combi_postalcode == COMBI_POSTALCODE_ALL:
+		ages = generalise_age_decade(user.age)
+		users = User.objects.filter(age__in=ages).order_by('age', 'postalcode')
+		return users
+
+	if combi_age == COMBI_AGE_ALL and combi_postalcode == COMBI_POSTALCODE_EXACT:
+		users = User.objects.filter(postalcode=user.postalcode).order_by('age', 'postalcode')
+		return users
+
+	if combi_age == COMBI_AGE_ALL and combi_postalcode == COMBI_POSTALCODE_SECTOR:
+		postalcode = str(user.postalcode)
+		postalcode = postalcode[:2]
+		users = User.objects.filter(postalcode__startswith=postalcode).order_by('age', 'postalcode')
+		return users
+
+	if combi_age == COMBI_AGE_ALL and combi_postalcode == COMBI_POSTALCODE_ALL:
+		users = User.objects.all().order_by('age', 'postalcode')
+		return users
+
+def generalise_date_and_check_record_exists(combi_date, patient):
+	duration = timezone.now()
+
+	if combi_date == COMBI_DATE_LM:
+		duration = duration - timedelta(days=30)
+
+	if combi_date == COMBI_DATE_LY:
+		duration = duration - timedelta(days=365)
+
+	if combi_date == COMBI_DATE_LM or combi_date == COMBI_DATE_LY: # LM or LY
+		bp_readings = patient.readings_patient.filter(type='Blood Pressure', timestamp__gte=duration)
+		hr_readings = patient.readings_patient.filter(type='Heart Rate', timestamp__gte=duration)
+		temp_readings = patient.readings_patient.filter(type='Temperature', timestamp__gte=duration)
+
+		if bp_readings.count() != 0 or hr_readings.count() != 0 or temp_readings.count() != 0:
+			return True
+		return False
+
+	else:
+		bp_readings = patient.readings_patient.filter(type='Blood Pressure')
+		hr_readings = patient.readings_patient.filter(type='Heart Rate')
+		temp_readings = patient.readings_patient.filter(type='Temperature')
+
+		if bp_readings.count() != 0 or hr_readings.count() != 0 or temp_readings.count() != 0:
+			return True
+		return False
---
+++ adminlogin/anonymise_helper.py
@@ -0,0 +1,254 @@
+### Helper functions related to anonymisation
+
+AGE_NAME = "age"
+POSTALCODE_NAME = "postalcode"
+DATE_NAME = "date"
+
+COMBI_AGE_EXACT = 'A'
+COMBI_AGE_DECADE = 'D'
+COMBI_AGE_ALL = '*'
+COMBI_POSTALCODE_EXACT = 'P'
+COMBI_POSTALCODE_SECTOR = 'XX'
+COMBI_POSTALCODE_ALL = '*'
+COMBI_DATE_LM = 'LM'
+COMBI_DATE_LY = 'LY'
+COMBI_DATE_ALL = '*'
+
+QI_COMBI = {
+	##### Uncomment for final code #####
+	# 11: { # L1_1
+	# 	AGE_NAME: COMBI_AGE_EXACT,
+	# 	POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+	# 	DATE_NAME: COMBI_DATE_LM
+	# },
+
+	# 21: {
+	# 	AGE_NAME: COMBI_AGE_DECADE,
+	# 	POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+	# 	DATE_NAME: COMBI_DATE_LM
+	# },
+
+	# 22: {
+	# 	AGE_NAME: COMBI_AGE_EXACT,
+	# 	POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+	# 	DATE_NAME: COMBI_DATE_LM
+	# },
+
+	# 23: {
+	# 	AGE_NAME: COMBI_AGE_EXACT,
+	# 	POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+	# 	DATE_NAME: COMBI_DATE_LY
+	# },
+
+	# 31: {
+	# 	AGE_NAME: COMBI_AGE_ALL,
+	# 	POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+	# 	DATE_NAME: COMBI_DATE_LM
+	# },
+
+	32: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_LM
+	},
+
+	33: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	34: {
+		AGE_NAME: COMBI_AGE_EXACT,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_LM 
+	},
+
+	35: {
+		AGE_NAME: COMBI_AGE_EXACT,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	36: {
+		AGE_NAME: COMBI_AGE_EXACT,
+		POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	41: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_LM
+	},
+
+	42: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	43: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_LM
+	},
+
+	44: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	45: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	46: {
+		AGE_NAME: COMBI_AGE_EXACT,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	47: {
+		AGE_NAME: COMBI_AGE_EXACT,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	51: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_LM
+	},
+
+	52: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	53: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_EXACT,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	54: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	55: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	56: {
+		AGE_NAME: COMBI_AGE_EXACT,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	61: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_LY
+	},
+
+	62: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_SECTOR,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	63: {
+		AGE_NAME: COMBI_AGE_DECADE,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_ALL
+	},
+
+	71: {
+		AGE_NAME: COMBI_AGE_ALL,
+		POSTALCODE_NAME: COMBI_POSTALCODE_ALL,
+		DATE_NAME: COMBI_DATE_ALL
+	}
+}
+
+def generalise_age_decade(age):
+	ages = []
+
+	if 0 <= age <= 10:
+		ages = list(range(0, 11))
+		return ages
+
+	if 11 <= age <= 20:
+		ages = list(range(11, 21))
+		return ages
+
+	if 21 <= age <= 30:
+		ages = list(range(21, 31))
+		return ages
+
+	if 31 <= age <= 40:
+		ages = list(range(31, 41))
+		return ages
+
+	if 41 <= age <= 50:
+		ages = list(range(41, 51))
+		return ages
+
+	if 51 <= age <= 60:
+		ages = list(range(51, 61))
+		return ages
+
+	if 61 <= age <= 70:
+		ages = list(range(61, 71))
+		return ages
+
+	if 71 <= age <= 80:
+		ages = list(range(71, 81))
+		return ages
+
+	if 81 <= age <= 90:
+		ages = list(range(81, 91))
+		return ages
+
+	if 91 <= age <= 100:
+		ages = list(range(91, 101))
+		return ages
+
+def map_age_to_decade(age):
+	if 0 <= age <= 10:
+		return '0-10'
+
+	if 11 <= age <= 20:
+		return '11-20'
+
+	if 21 <= age <= 30:
+		return '21-30'
+
+	if 31 <= age <= 40:
+		return '31-40'
+
+	if 41 <= age <= 50:
+		return '41-50'
+
+	if 51 <= age <= 60:
+		return '51-60'
+
+	if 61 <= age <= 70:
+		return '61-70'
+
+	if 71 <= age <= 80:
+		return '71-80'
+
+	if 81 <= age <= 90:
+		return '81-90'
+
+	if 91 <= age <= 100:
+		return '91-100'
