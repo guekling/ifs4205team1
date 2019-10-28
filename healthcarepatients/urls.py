a77136a5b1f2bef4bd7a70a46d50ecff4083f280
Yeo Guek Ling <yeoyueling@gmail.com>
2019-10-29 00:27:14 +0800

    Add Download Link for Medical Records & Healthcare Notes (#12)
    * Change class views name to avoid name conflicts
    
    * Add download link for records and healthcare notes
    
    * Set testing view & url for serving protected media

--- healthcarepatients/urls.py
+++ healthcarepatients/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path("<uuid:healthcare_id>/patients/<uuid:patient_id>", views.show_patient, name="show_patient"),
   path("<uuid:healthcare_id>/patients/<uuid:patient_id>/records", views.show_patient_records, name="show_patient_records"),
   path("<uuid:healthcare_id>/patients/<uuid:patient_id>/records/<uuid:record_id>", views.show_patient_record, name="show_patient_record"),
+  path("<uuid:healthcare_id>/patients/<uuid:patient_id>/records/<uuid:record_id>/download", views.download_patient_record, name="download_patient_record"),
   path("<uuid:healthcare_id>/patients/<uuid:patient_id>/transfer", views.transfer_patient, name="transfer_patient"),
 ]
