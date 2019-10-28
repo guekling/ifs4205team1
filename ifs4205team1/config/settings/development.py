f090459fc575f6ed9d063d36b91444548da46983
Yeo Guek Ling <yeoyueling@gmail.com>
2019-10-28 12:33:48 +0800

    Add Logs & SSL/TLS Connections to DBs, Fix Migrations for Researcher DB (#11)
    * Configure SSL/TLS for databases & Add Logging
    
    * Fix researcherquery migrations

--- ifs4205team1/config/settings/development.py
+++ ifs4205team1/config/settings/development.py
@@ -10,6 +10,41 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ.get("DB_NAME"),
+    'USER': os.environ.get("DB_USER"),
+    'PASSWORD': os.environ.get("DB_PASS"),
+    'HOST': os.environ.get("DB_HOST"),
+    'PORT': '5432',
+  },
+  'safedb': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ.get("DB_NAME2"),
+    'USER': os.environ.get("DB_USER2"),
+    'PASSWORD': os.environ.get("DB_PASS2"),
+    'HOST': os.environ.get("DB_HOST2"),
+    'PORT': '5432',
+  },
+  'logdb': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ.get("DB_NAME3"),
+    'USER': os.environ.get("DB_USER3"),
+    'PASSWORD': os.environ.get("DB_PASS3"),
+    'HOST': os.environ.get("DB_HOST3"),
+    'PORT': '5432',
+  }
+}
+
+DATABASE_ROUTERS = [
+  'researcherquery.router.ResearcherqueryRouter',
+  'userlogs.router.UserlogsRouter'
+]
+
 # Media files
 
 MEDIA_URL = os.environ.get("MEDIA_URL")
